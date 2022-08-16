d61575679c09de6d588f75785c0b62c1822456ce
Mantis <barabanovpv95@gmail.com>
2022-08-16 14:10:40 +0300

    feat: complete class MultipleBrands

--- multiple_brands_handler.py
+++ multiple_brands_handler.py
@@ -1,9 +1,11 @@
 import os
 import pandas as pd
+from common_words_tokens import CommonWords
 
 
-class MultipleBrands:
+class MultipleBrands(CommonWords):
     def __init__(self, path_to_client_data: str, path_to_base_data: str):
+        super().__init__(path_to_client_data, path_to_base_data)
         assert os.path.exists(path_to_client_data), \
             f'{path_to_client_data} do not exists'
         assert os.path.exists(path_to_base_data), \
@@ -28,7 +30,10 @@ class MultipleBrands:
         elif 'brand_name' not in df:
             raise Exception('No "brand_name" column')
 
-        return df['client_product_title', 'ean_code', 'brand_name']
+        df = df['client_product_title', 'ean_code', 'brand_name'] \
+            .sort_values(by=['brand_name', 'client_product_title'])
+
+        return df
 
     def make_base_df(self):
         """
@@ -43,4 +48,72 @@ class MultipleBrands:
         elif 'brand_name' not in df:
             raise Exception('No "brand_name" column')
 
-        return df['base_product_title', 'product_id', 'brand_name']
+        df = df['base_product_title', 'product_id', 'brand_name']\
+            .sort_values(by=['brand_name', 'base_product_title'])
+
+        return df
+
+    def cross_join_table(self):
+        df_cross = \
+            self.__client_df['client_product_title', 'brand_name'].merge(
+                self.__base_df['base_product_title', 'brand_name'],
+                on='brand_name',
+                how='outer'
+            )
+
+        return df_cross
+
+    def calc_cw_dataframe(self) -> pd.DataFrame:
+        """
+        Applying calc_common_words to all possible variations
+        :returns dataframe columns: client_product_title, base_product_title,
+        common_words
+        """
+        df = self.cross_join_table()
+        df['common_words'] = df.apply(
+            lambda x: self.calc_common_words(x['client_product_title'],
+                                             x['base_product_title']),
+            axis=1
+        )
+        return df
+
+    def filter_df_by_length_of_sets(self) -> pd.DataFrame:
+        """
+        Filtering dataframe with sets of max length for each product
+        :return: filtered df with ean code
+        """
+
+        df = self.cross_join_table()
+        grouped_df = df.groupby(
+            'client_product_title', 'brand_name', as_index=False
+        ).agg({
+            'common_words': self.max_set_length
+        })
+
+        res_df = grouped_df.merge(
+            df,
+            how='inner',
+            on=['client_product_title', 'common_words']
+        )
+
+        return res_df
+
+    def make_result_df(self) -> pd.DataFrame:
+        """
+        Join EAN_CODE
+        Join product_id
+        :return: full DataFrame
+        """
+        df = self.filter_df_by_length_of_sets()
+        res_df_with_ean = df.merge(
+            self.__client_df['ean_code'],
+            how='left',
+            on='client_product_title'
+        )
+
+        res_df_with_sku = res_df_with_ean.merge(
+            self.__base_df['product_id'],
+            how='left',
+            on='base_product_title'
+        )
+        return res_df_with_sku
